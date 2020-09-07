a386cf0337e741546b526958c69223f6e1611167
joseph-nanuda <lsj@widaehan.com>
2020-09-07 12:22:29 +0900

    Product consult CRU

--- src/modules/product-consult/product.service.ts
+++ src/modules/product-consult/product.service.ts
@@ -1,11 +1,90 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { BaseService } from 'src/core';
+import {
+  AdminProductConsultListDto,
+  AdminProductConsultUpdateDto,
+} from './dto';
+import { PaginatedRequest, PaginatedResponse } from 'src/common';
+import { ProductConsult } from './product-consult.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ProductConsultModule } from './product-consult.module';
 
 @Injectable()
 export class ProductConsultService extends BaseService {
-  constructor() {
+  constructor(
+    @InjectRepository(ProductConsult)
+    private readonly productConsultRepo: Repository<ProductConsult>,
+  ) {
     super();
   }
 
-  //   async findAllForAdmin()
+  /**
+   * find all for admin
+   * @param adminProductConsutListDto
+   * @param pagination
+   */
+  async findAllForAdmin(
+    adminProductConsutListDto: AdminProductConsultListDto,
+    pagination: PaginatedRequest,
+  ): Promise<PaginatedResponse<ProductConsult>> {
+    const qb = this.productConsultRepo
+      .createQueryBuilder('productConsult')
+      .CustomLeftJoinAndSelect(['admin'])
+      .CustomInnerJoinAndSelect(['codeManagement', 'addressInfo'])
+      .AndWhereLike(
+        'productConsult',
+        'addressCode',
+        adminProductConsutListDto.addressCode,
+        adminProductConsutListDto.exclude('addressCode'),
+      )
+      .AndWhereLike(
+        'admin',
+        'name',
+        adminProductConsutListDto.adminName,
+        adminProductConsutListDto.exclude('adminName'),
+      )
+      .AndWhereEqual(
+        'productConsult',
+        'pConsultManager',
+        adminProductConsutListDto.adminNo,
+        adminProductConsutListDto.exclude('adminNo'),
+      )
+      .WhereAndOrder(adminProductConsutListDto)
+      .Paginate(pagination);
+
+    const [items, totalCount] = await qb.getManyAndCount();
+    return { items, totalCount };
+  }
+
+  /**
+   * find for admin
+   * @param productConsultNo
+   */
+  async findOneForAdmin(productConsultNo: number): Promise<ProductConsult> {
+    const consult = await this.productConsultRepo
+      .createQueryBuilder('productConsult')
+      .CustomInnerJoinAndSelect(['codeManagement'])
+      .CustomLeftJoinAndSelect(['admin', 'addressInfo'])
+      .where('productConsult.no = :no', { no: productConsultNo })
+      .getOne();
+    if (!consult) {
+      throw new NotFoundException();
+    }
+    return consult;
+  }
+
+  /**
+   * update product consult
+   * @param productConsultNo
+   * @param adminProductConsultUpdateDto
+   */
+  async updateForAdmin(
+    productConsultNo: number,
+    adminProductConsultUpdateDto: AdminProductConsultUpdateDto,
+  ): Promise<ProductConsult> {
+    let consult = await this.productConsultRepo.findOne(productConsultNo);
+    consult = consult.set(adminProductConsultUpdateDto);
+    return consult;
+  }
 }
