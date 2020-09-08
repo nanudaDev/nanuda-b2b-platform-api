a76d0e5bde1de65e3cd4c5ec488a8ca15a0eb069
joseph-nanuda <lsj@widaehan.com>
2020-09-08 18:32:05 +0900

    지점 검색 리팩토링

--- src/modules/company-district/nanuda-company-district.service.ts
+++ src/modules/company-district/nanuda-company-district.service.ts
@@ -5,6 +5,11 @@ import { CompanyDistrict } from './company-district.entity';
 import { Repository } from 'typeorm';
 import { CompanyDistrictListDto } from './dto';
 
+export class SearchResults {
+  cities?: CompanyDistrict[];
+  regions?: CompanyDistrict[];
+  districts?: CompanyDistrict[];
+}
 @Injectable()
 export class NanudaCompanyDistrictService extends BaseService {
   constructor(
@@ -20,14 +25,52 @@ export class NanudaCompanyDistrictService extends BaseService {
    */
   async search(
     companyDistrictListDto: CompanyDistrictListDto,
-  ): Promise<CompanyDistrict[]> {
-    const qb = await this.companyDistrictRepo
+  ): Promise<SearchResults> {
+    const searchResults = new SearchResults();
+    const cities = await this.companyDistrictRepo
       .createQueryBuilder('companyDistrict')
-      .AndWhereLike('companyDistrict', 'address', companyDistrictListDto.nameKr)
+      .CustomInnerJoinAndSelect(['deliverySpaces'])
       .where('companyDistrict.companyDistrictStatus = :companyDistrictStatus', {
         companyDistrictStatus: APPROVAL_STATUS.APPROVAL,
       })
+      .AndWhereLike(
+        'companyDistrict',
+        'region1DepthName',
+        companyDistrictListDto.keyword,
+      )
+      .limit(2)
       .getMany();
-    return qb;
+    searchResults.cities = cities;
+
+    const regions = await this.companyDistrictRepo
+      .createQueryBuilder('companyDistrict')
+      .CustomInnerJoinAndSelect(['deliverySpaces'])
+      .where('companyDistrict.companyDistrictStatus = :companyDistrictStatus', {
+        companyDistrictStatus: APPROVAL_STATUS.APPROVAL,
+      })
+      .AndWhereLike(
+        'companyDistrict',
+        'region2DepthName',
+        companyDistrictListDto.keyword,
+      )
+      .limit(2)
+      .getMany();
+    searchResults.regions = regions;
+
+    // const districts = await this.companyDistrictRepo
+    //   .createQueryBuilder('companyDistrict')
+    //   .CustomInnerJoinAndSelect(['deliverySpaces'])
+    //   .where('companyDistrict.companyDistrictStatus = :companyDistrictStatus', {
+    //     companyDistrictStatus: APPROVAL_STATUS.APPROVAL,
+    //   })
+    //   .AndWhereLike(
+    //     'companyDistrict',
+    //     'address',
+    //     companyDistrictListDto.keyword,
+    //   )
+    //   .limit(2)
+    //   .getMany();
+    // searchResults.districts = districts;
+    return searchResults;
   }
 }
