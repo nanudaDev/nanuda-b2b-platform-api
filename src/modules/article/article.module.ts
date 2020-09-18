75d9089ff9d668b09655fc970eb7a2f120fb4e56
joseph-nanuda <lsj@widaehan.com>
2020-09-18 15:19:08 +0900

    nanuda article controller

--- src/modules/article/article.module.ts
+++ src/modules/article/article.module.ts
@@ -5,10 +5,11 @@ import { FileUploadModule } from '../file-upload/file-upload.module';
 import { ArticleService } from './article.service';
 import { NanudaArticleService } from './nanuda-article.service';
 import { AdminArticleController } from './admin-article.controller';
+import { NanudaArticleController } from './nanuda-article.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Article]), FileUploadModule],
-  controllers: [AdminArticleController],
+  controllers: [AdminArticleController, NanudaArticleController],
   providers: [ArticleService, NanudaArticleService],
 })
 export class ArticleModule {}
