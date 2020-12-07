3610ac4d5b2df3e78ac642acf74e39e139ea9374
bbogeul <illumeweb@gmail.com>
2020-12-07 17:35:12 +0900

    send edited message

--- src/modules/message-delivery-space/admin-message-delivery-space.controller.ts
+++ src/modules/message-delivery-space/admin-message-delivery-space.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   Controller,
   Get,
   Param,
@@ -12,12 +13,12 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { AuthRolesGuard, BaseController, CONST_ADMIN_USER } from 'src/core';
 import { MessageDeliverySpaceService } from './admin-message-delivery-space.service';
-import { MessageFloatingPopulationDto } from './dto';
+import { EditedMessageDto } from './dto';
 
 @Controller()
 @ApiTags('ADMIN MESSAGE')
-// @UseGuards(new AuthRolesGuard(...CONST_ADMIN_USER))
-// @ApiBearerAuth()
+@UseGuards(new AuthRolesGuard(...CONST_ADMIN_USER))
+@ApiBearerAuth()
 export class AdminMessageDeliverySpaceController extends BaseController {
   constructor(
     private readonly messageDeliverySpaceService: MessageDeliverySpaceService,
@@ -30,12 +31,23 @@ export class AdminMessageDeliverySpaceController extends BaseController {
    * @param companyDistrictNo
    */
   @Get('/admin/message-delivery-space/:id([0-9]+)')
-  async sendMessage(
+  async getMessage(
     @Param('id', ParseIntPipe) deliveryFounderConsultNo: number,
-    @Req() req: Request,
   ) {
     return await this.messageDeliverySpaceService.sendMessageAndPlaceInIndex(
       deliveryFounderConsultNo,
+    );
+  }
+
+  @Post('/admin/send-message/:nanudaUserNo([0-9]+)')
+  async sendEditedMessage(
+    @Param('nanudaUserNo', ParseIntPipe) nanudaUserNo: number,
+    @Body() editedMessageDto: EditedMessageDto,
+    @Req() req: Request,
+  ) {
+    return await this.messageDeliverySpaceService.sendEditedMessage(
+      nanudaUserNo,
+      editedMessageDto,
       req,
     );
   }
