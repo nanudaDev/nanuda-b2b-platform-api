5d98bf2686b3a4bf551e13956e6a921c899f9485
bbogeul <illumeweb@gmail.com>
2021-01-12 15:13:26 +0900

    리스트 업체 상태값 조인

--- src/modules/delivery-space-nnd-brand-op-record/delivery-space-nnd-brand-op-record.module.ts
+++ src/modules/delivery-space-nnd-brand-op-record/delivery-space-nnd-brand-op-record.module.ts
@@ -1,12 +1,11 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { DeliverySpaceNndBrandOpRecord } from "./delivery-space-nnd-brand-op-record.entity";
-import { DeliverySpaceNndBrandOpRecordService } from "./delivery-space-nnd-brand-op-record.service";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DeliverySpaceNndBrandOpRecord } from './delivery-space-nnd-brand-op-record.entity';
+import { DeliverySpaceNndBrandOpRecordService } from './delivery-space-nnd-brand-op-record.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([DeliverySpaceNndBrandOpRecord])],
-    providers: [DeliverySpaceNndBrandOpRecordService],
-    exports: [DeliverySpaceNndBrandOpRecordService]
+  imports: [TypeOrmModule.forFeature([DeliverySpaceNndBrandOpRecord])],
+  providers: [DeliverySpaceNndBrandOpRecordService],
+  exports: [DeliverySpaceNndBrandOpRecordService],
 })
-
 export class DeliverySpaceNndBrandOpRecordModule {}
