2dab44b6050a5e2fe17944a2bf5240ed8384b134
eddie-nanuda <lsh@widaehan.com>
2021-01-17 13:38:49 +0900

    오타 수정

--- src/core/utils/nanuda-sms-notification.service.ts
+++ src/core/utils/nanuda-sms-notification.service.ts
@@ -196,7 +196,7 @@ export class NanudaSmsNotificationService {
     const body = {
       receiver: attendees.phone,
       sender: process.env.ALIGO_SENDER_PHONE,
-      msg: `[나누디키친] 안녕하세요 ${attendees.name}님, 나누다키친입니다. \n나누다키친 창업 설명회에 신청해주셔서 감사드립니다. \n\n창업 설명회 안내 \n\n일시: ${presentationDate} | ${attendees.scheduleTime} \n장소: http://naver.me/5Yn0UdYx \n서울 서초구 서초대로 77길 55 에이프로스퀘어 빌딩 6층
+      msg: `[나누다키친] 안녕하세요 ${attendees.name}님, 나누다키친입니다. \n나누다키친 창업 설명회에 신청해주셔서 감사드립니다. \n\n창업 설명회 안내 \n\n일시: ${presentationDate} | ${attendees.scheduleTime} \n장소: http://naver.me/5Yn0UdYx \n서울 서초구 서초대로 77길 55 에이프로스퀘어 빌딩 6층
         (강남역,신논현역 도보 이동 가능) \n※ 건물에 주차가 어려울 수 있으니 주변 공영주차장을 이용해주시면 감사하겠습니다. \n\n감사합니다. \n나누다키친 드림. \n\nTEL:02-556-5777 \n무료 거부 080-870-0727`,
       title: '안녕하세요 위대한상사입니다.',
     };
@@ -300,8 +300,8 @@ export class NanudaSmsNotificationService {
     const body = {
       receiver: attendees.phone,
       sender: process.env.ALIGO_SENDER_PHONE,
-      msg: `[나누디키친] 안녕하세요 ${attendees.name}님, 나누다키친입니다. \n나누다키친 온라인 창업 설명회에 신청해주셔서 감사드립니다. \n\n온라인 창업설명회 안내드립니다. \n\n날짜: ${todayDate}\n시간: ${attendees.eventTime}\n이용방법: 줌(ZOOM) 사이트 또는 앱으로 간편하게 화상으로 창업설명회에 참여 가능합니다.\n\n문의사항이 있으신 경우 해당 번호로 연락주시면 상담 도와드리겠습니다.\n\n나누다키친 영업시간 : 평일 10시~18시\n감사합니다 \n나누다키친 드림. \n\nTEL: ${process.env.ALIGO_SECONDARY_PHONE}`,
-      title: '안녕하세요 나누디키친입니다.',
+      msg: `[나누다키친] 안녕하세요 ${attendees.name}님, 나누다키친입니다. \n나누다키친 온라인 창업 설명회에 신청해주셔서 감사드립니다. \n\n온라인 창업설명회 안내드립니다. \n\n날짜: ${todayDate}\n시간: ${attendees.eventTime}\n이용방법: 줌(ZOOM) 사이트 또는 앱으로 간편하게 화상으로 창업설명회에 참여 가능합니다.\n\n문의사항이 있으신 경우 해당 번호로 연락주시면 상담 도와드리겠습니다.\n\n나누다키친 영업시간 : 평일 10시~18시\n감사합니다 \n나누다키친 드림. \n\nTEL: ${process.env.ALIGO_SECONDARY_PHONE}`,
+      title: '안녕하세요 나누다키친입니다.',
     };
 
     return { body, auth };
