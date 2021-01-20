61353919d9cdfaadddd5aebeb6c365d699a6a416
bbogeul <illumeweb@gmail.com>
2021-01-20 17:22:13 +0900

    initial commit for create delivery founder consult without login

---
+++ src/modules/delivery-founder-consult/dto/without-login-delivery-founder-consult-create.dto.ts
@@ -0,0 +1,62 @@
+import { BaseDto } from 'src/core';
+import { ApiPropertyOptional, ApiProperty } from '@nestjs/swagger';
+import {
+  IsOptional,
+  IsNotEmpty,
+  IsEnum,
+  IsPhoneNumber,
+  IsArray,
+} from 'class-validator';
+import { Expose } from 'class-transformer';
+import { AVAILABLE_TIME, Default } from 'src/common';
+
+export class WithoutLoginDeliveryFounderConsultCreateDto extends BaseDto<
+  WithoutLoginDeliveryFounderConsultCreateDto
+> {
+  @ApiProperty()
+  @IsNotEmpty()
+  @Expose()
+  name: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @Expose()
+  @IsPhoneNumber('KR')
+  phone: string;
+
+  @ApiProperty({ type: Number, isArray: true })
+  @IsNotEmpty()
+  @Expose()
+  @IsArray()
+  deliverySpaceNos?: number[];
+
+  @ApiPropertyOptional({ enum: AVAILABLE_TIME })
+  @IsOptional()
+  @Expose()
+  @Default(AVAILABLE_TIME.ALL)
+  @IsEnum(AVAILABLE_TIME)
+  hopeTime?: AVAILABLE_TIME;
+
+  nanudaUserNo?: number;
+
+  //   @ApiPropertyOptional({ enum: AVAILABLE_TIME })
+  //   @IsOptional()
+  //   @Expose()
+  //   @Default(AVAILABLE_TIME.ALL)
+  //   @IsEnum(AVAILABLE_TIME)
+  //   hopeTime?: AVAILABLE_TIME;
+
+  //   @ApiPropertyOptional({ enum: YN })
+  //   @IsOptional()
+  //   @Expose()
+  //   @Default(YN.NO)
+  //   @IsEnum(YN)
+  //   changUpExpYn?: YN;
+
+  //   @ApiPropertyOptional({ enum: YN })
+  //   @IsOptional()
+  //   @Expose()
+  //   @Default(YN.NO)
+  //   @IsEnum(YN)
+  //   spaceOwnYn?: YN;
+}
