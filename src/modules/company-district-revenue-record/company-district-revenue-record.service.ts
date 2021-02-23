329e9f36ac0a2ce927ce00a96dabbcb582c6f520
eddie-nanuda <lsh@widaehan.com>
2021-02-23 11:45:55 +0900

    refactoring nate

--- src/modules/company-district-revenue-record/company-district-revenue-record.service.ts
+++ src/modules/company-district-revenue-record/company-district-revenue-record.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   ForbiddenException,
   Injectable,
   NotAcceptableException,
@@ -30,24 +31,11 @@ export class CompanyDistrictRevenueRecordService extends BaseService {
     super();
   }
 
-  //companyUser가 속한 company의 district
-  buildOnlyCompanyDistrictBelong(
-    companyUser: CompanyUser,
-  ): SelectQueryBuilder<CompanyDistrictRevenueRecord> {
-    const qb = this.companyDistrictRevenueRecordRepo
-      .createQueryBuilder('revenueRecord')
-      // .CustomInnerJoinAndSelect(['companyDistrict'])
-      .innerJoin('revenueRecord.companyDistrict', 'companyDistrict')
-      .innerJoin('companyDistrict.company', 'company')
-      .where('company.no = :companyNo', { companyNo: companyUser.companyNo });
-    return qb;
-  }
-
   async findOne(
     companyUser: CompanyUser,
     no: number,
   ): Promise<CompanyDistrictRevenueRecord> {
-    return this.buildOnlyCompanyDistrictBelong(companyUser)
+    return this.__build_record(companyUser)
       .andWhere('revenueRecord.no = :recordNo', { recordNo: no })
       .getOne();
   }
@@ -56,7 +44,7 @@ export class CompanyDistrictRevenueRecordService extends BaseService {
     companyUser: CompanyUser,
     companyDistrictRevenueRecordListDto: CompanyDistrictRevenueRecordListDto,
   ): Promise<CompanyDistrictRevenueRecord[]> {
-    const qb = this.buildOnlyCompanyDistrictBelong(companyUser).AndWhereEqual(
+    const qb = this.__build_record(companyUser).AndWhereEqual(
       'companyDistrict',
       'no',
       companyDistrictRevenueRecordListDto.companyDistrictNo,
@@ -72,22 +60,28 @@ export class CompanyDistrictRevenueRecordService extends BaseService {
     return records;
   }
 
+  /**
+   * create new record
+   * @param companyUser
+   * @param companyDistrictRevenueRecordCreateDto
+   */
   async createRecord(
     companyUser: CompanyUser,
     companyDistrictRevenueRecordCreateDto: CompanyDistrictRevenueRecordCreateDto,
   ): Promise<CompanyDistrictRevenueRecord> {
     // 해당 district가 companyUser의 district 일때만 save 함
-    const theDistricts = await this.companyDistrictRepo
+    const theDistrict = await this.companyDistrictRepo
       .createQueryBuilder('companyDistrict')
       .where('companyDistrict.companyNo = :companyNo', {
         companyNo: companyUser.companyNo,
       })
-      .getMany();
-    const isBelongToCompany = theDistricts.some(
-      e => e.no == companyDistrictRevenueRecordCreateDto.companyDistrictNo,
-    );
-    if (!isBelongToCompany) {
-      throw new ForbiddenException();
+      .andWhere('companyDistrict.no = :no', {
+        no: companyDistrictRevenueRecordCreateDto.companyDistrictNo,
+      })
+      .getOne();
+
+    if (!theDistrict) {
+      throw new BadRequestException('NOT YOUR DISTRICT');
     }
     return await this.companyDistrictRevenueRecordRepo.save(
       companyDistrictRevenueRecordCreateDto,
@@ -101,7 +95,7 @@ export class CompanyDistrictRevenueRecordService extends BaseService {
   ): Promise<CompanyDistrictRevenueRecord> {
     const requestedRecord = await this.findOne(companyUser, id);
     if (!requestedRecord) {
-      throw new ForbiddenException();
+      throw new NotFoundException();
     }
     requestedRecord.set(companyDistrictRevenueRecordUpdateDto);
     return await this.companyDistrictRevenueRecordRepo.save(requestedRecord);
@@ -128,5 +122,17 @@ export class CompanyDistrictRevenueRecordService extends BaseService {
     qb.orderBy('revenueRecord.year', 'DESC');
     const records = await qb.getMany();
     return records;
+  }
+
+  private __build_record(
+    companyUser: CompanyUser,
+  ): SelectQueryBuilder<CompanyDistrictRevenueRecord> {
+    const qb = this.companyDistrictRevenueRecordRepo
+      .createQueryBuilder('revenueRecord')
+      // .CustomInnerJoinAndSelect(['companyDistrict'])
+      .innerJoin('revenueRecord.companyDistrict', 'companyDistrict')
+      .innerJoin('companyDistrict.company', 'company')
+      .where('company.no = :companyNo', { companyNo: companyUser.companyNo });
+    return qb;
   }
 }
